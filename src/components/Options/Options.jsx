6e25a0606399fe206120cf841a3e4ace39a1ff72
Sergii Marchenko <marikmarchelo@gmail.com>
2024-08-15 15:16:49 +0300

    add 2 components

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,9 @@
+export default function Options() {
+  return (
+    <div>
+      <button style={{ backgroundColor: "gray" }}>Good</button>
+      <button style={{ backgroundColor: "gray" }}>Neutral</button>
+      <button style={{ backgroundColor: "gray" }}>Bad</button>
+    </div>
+  );
+}
